57675ebcabbefee98d4771f3be19d5958175d5c1
A Greaves <anna@codeinstitute.net>
2021-09-29 11:35:52 +0200

    Update bootstrap classes and image link.

--- 06-starter-code/SignUpForm.js
+++ 06-starter-code/SignUpForm.js
@@ -10,7 +10,7 @@ import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 const SignUpForm = () => {
   return (
     <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-0 p-md-2" md={6}>
+      <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
@@ -25,12 +25,12 @@ const SignUpForm = () => {
       </Col>
       <Col
         md={6}
-        className={`my-auto d-none d-md-block p-0 p-md-2 ${styles.SignUpCol}`}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
       >
         <Image
           className={`${appStyles.FillerImage}`}
           src={
-            "https://res.cloudinary.com/drlqahj5d/image/upload/v1630595819/hero2_ylztrr.jpg"
+            "https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"
           }
         />
       </Col>
