de65e5782776b6880ab061ae5c3c6fa16d8e6d17
A Greaves <anna.j.greaves@gmail.com>
2021-10-11 15:11:04 +0000

    Make changes to 13 starter code.

--- 13-starter-code/PostCreateForm.js
+++ 13-starter-code/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -6,11 +6,15 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 
+import Upload from "../../assets/upload.png";
+
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
 function PostCreateForm() {
+
+  const [errors, setErrors] = useState({});
 
 //   Add your handleChange function here
 
@@ -46,7 +50,7 @@ function PostCreateForm() {
                   className="d-flex justify-content-center"
                   htmlFor="image-upload"
                 >
-
+                  ASSET
                 </Form.Label>
 
             </Form.Group>
