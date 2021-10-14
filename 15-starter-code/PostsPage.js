af1fbcbbd9bfc5f4d7de7e2a1951425498e5323c
A Greaves <anna.j.greaves@gmail.com>
2021-10-14 11:31:20 +0000

    Add starter code for lesson 15

---
+++ 15-starter-code/PostsPage.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsPage.module.css";
+
+function PostsPage() {
+  
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles mobile</p>
+        
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Popular profiles for desktop</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default PostsPage;
