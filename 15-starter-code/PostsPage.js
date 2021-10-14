895ad84985a12c88d1e1f17987f159b42b5efba9
A Greaves <anna.j.greaves@gmail.com>
2021-10-14 13:36:40 +0000

    Update placeholder text

--- 15-starter-code/PostsPage.js
+++ 15-starter-code/PostsPage.js
@@ -15,7 +15,7 @@ function PostsPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        
+        <p>List of posts here</p>
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
