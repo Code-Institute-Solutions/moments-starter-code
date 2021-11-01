73a3176af1103871e669795432e25fd00219262e
A Greaves <anna.j.greaves@gmail.com>
2021-11-01 16:20:31 +0000

    Add starter code for video 23

---
+++ 23-starter-code/ProfilePage.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import Asset from "../../components/Asset";
+
+import styles from "../../styles/ProfilePage.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+import PopularProfiles from "./PopularProfiles";
+
+function ProfilePage() {
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  useEffect(() => {
+      setHasLoaded(true);
+  }, [hasLoaded])
+
+  const mainProfile = (
+    <>
+      <Row noGutters className="px-3 text-center">
+        <Col lg={3} className="text-lg-left">
+          <p>Image</p>
+        </Col>
+        <Col lg={6}>
+          <h3 className="m-2">Profile username</h3>
+          <p>Profile stats</p>
+        </Col>
+        <Col lg={3} className="text-lg-right">
+        <p>Follow button</p>
+        </Col>
+        <Col className="p-3">Profile content</Col>
+      </Row>
+    </>
+  );
+
+  const mainProfilePosts = (
+    <>
+      <hr />
+      <p className="text-center">Profile owner's posts</p>
+      <hr />
+    </>
+  );
+
+  return (
+    <Row>
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <PopularProfiles mobile />
+        <Container className={appStyles.Content}>
+          {hasLoaded ? (
+            <>
+              {mainProfile}
+              {mainProfilePosts}
+            </>
+          ) : (
+            <Asset spinner />
+          )}
+        </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+      </Col>
+    </Row>
+  );
+}
+
+export default ProfilePage;
